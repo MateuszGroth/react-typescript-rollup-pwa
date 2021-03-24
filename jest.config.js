8ae2572992ae4e02e16c9e7a810970caa126a6e4
Mateusz Groth <Mgroth@adva.com>
2021-03-24 18:24:22 +0100

    jest conf

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
     'setupFilesAfterEnv': ['./setupTestFramework.js'],
     'testEnvironment': 'node',
@@ -5,5 +10,6 @@ module.exports = {
     'testMatch': ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
     'transform': {
         '^.+\\.(ts|tsx)$': 'ts-jest'
-    }
+    },
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
 };
